15f36807f93124c60304e3b56e3dcc301fd4e2f9
Xytriza <cryfxreal@gmail.com>
2024-05-26 00:02:26 -0700

    GDDP support

--- src/CreatorLayer.cpp
+++ src/CreatorLayer.cpp
@@ -4,6 +4,10 @@
 using namespace geode::prelude;
 
 class $modify(CreatorLayer) {
+	static void onModify(auto & self) {
+		static_cast<void>(self.setHookPriority("CreatorLayer::init", -43));
+	}
+
     bool init() {
         if (!CreatorLayer::init()) return false;
         if (!Mod::get()->getSettingValue<bool>("creatorlayer_enabled")) return true;
@@ -24,6 +28,7 @@ class $modify(CreatorLayer) {
         auto listsButton = creatorButtonsMenu->getChildByID("lists-button");
         auto mapPacksButton = creatorButtonsMenu->getChildByID("map-packs-button");
         auto searchButton = creatorButtonsMenu->getChildByID("search-button");
+        auto gddpButton = creatorButtonsMenu->getChildByID("demon-progression-button");
 
         versusButton->setVisible(false);
         mapButton->setVisible(false);
@@ -33,10 +38,21 @@ class $modify(CreatorLayer) {
         savedButton->setPosition(179.2, 244.6);
         scoresButton->setPosition(268.8, 244.6);
         pathsButton->setPosition(358.4, 244.6);
-        questsButton->setPosition(89.6, 155);
-        dailyButton->setPosition(179.2, 155);
-        weeklyButton->setPosition(268.8, 155);
-        gauntletsButton->setPosition(358.4, 155);
+
+        if (gddpButton) {
+            questsButton->setPosition(45.8, 155);
+            dailyButton->setPosition(135.4, 155);
+            weeklyButton->setPosition(225, 155);
+            gauntletsButton->setPosition(314.6, 155);
+            gddpButton->setPosition(404.2, 153);
+        }
+        else {
+            questsButton->setPosition(89.6, 155);
+            dailyButton->setPosition(179.2, 155);
+            weeklyButton->setPosition(268.8, 155);
+            gauntletsButton->setPosition(358.4, 155);
+        }
+
         featuredButton->setPosition(89.6, 65.4);
         listsButton->setPosition(179.2, 65.4);
         mapPacksButton->setPosition(268.8, 65.4);
